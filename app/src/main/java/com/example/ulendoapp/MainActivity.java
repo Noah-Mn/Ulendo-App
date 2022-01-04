a1aafd10d8450e9dac33eccdf576007f494964ee
Noah <bsc-com-09-17@cc.ac.mw>
2022-01-04 15:33:10 +0200

    commit

--- app/src/main/java/com/example/ulendoapp/MainActivity.java
+++ app/src/main/java/com/example/ulendoapp/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(() -> startActivity(new Intent(MainActivity.this, )),splashTime);
+        new Handler().postDelayed(() -> startActivity(new Intent(MainActivity.this, Login.class)),splashTime);
     }
 }
