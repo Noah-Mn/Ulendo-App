39743b0e7073433c26abde9d4b862b54e6209e46
Computer Science <bsc-com-09-17@unima.ac.mw>
2022-01-14 19:29:37 +0200

    commit

--- app/src/main/java/com/example/ulendoapp/Login.java
+++ app/src/main/java/com/example/ulendoapp/Login.java
@@ -1,23 +1,20 @@
 package com.example.ulendoapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthResult;
+import com.google.android.material.textview.MaterialTextView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class Login extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
@@ -25,7 +22,7 @@ public class Login extends AppCompatActivity {
     TextInputEditText loginEmail, loginPassword;
     TextInputLayout materialLogPassword, materialLogEmail;
     Button loginBtn;
-    TextView materialCreateAcc, materialForgotPasswd;
+    MaterialTextView materialCreateAcc, materialForgotPasswd;
     ProgressDialog progressDialog;
     String emailPattern = "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
             + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
@@ -50,31 +47,16 @@ public class Login extends AppCompatActivity {
         materialLogEmail = findViewById(R.id.materialLogEmail);
         materialLogPassword = findViewById(R.id.materialLogPassword);
 
-        materialForgotPasswd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Login.this,ForgotPassword.class));
-            }
-        });
-        materialCreateAcc.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Login.this, CreateAccount.class));
-            }
-        });
+        materialForgotPasswd.setOnClickListener(v -> startActivity(new Intent(Login.this,ForgotPassword.class)));
+        materialCreateAcc.setOnClickListener(v -> startActivity(new Intent(Login.this,Signup.class)));
 
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                performLogin();
-            }
-        });
+        loginBtn.setOnClickListener(v -> performLogin());
     }
     private void performLogin(){
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
-        String logEmailAddress = loginEmail.getText().toString();
-        String logPassword = loginPassword.getText().toString();
+        String logEmailAddress = Objects.requireNonNull(loginEmail.getText()).toString();
+        String logPassword = Objects.requireNonNull(loginPassword.getText()).toString();
 
         if (!logEmailAddress.matches(emailPattern)){
             materialLogEmail.setError("Please enter a valid email address");
@@ -88,26 +70,23 @@ public class Login extends AppCompatActivity {
             progressDialog.setCanceledOnTouchOutside(false);
             progressDialog.show();
 
-            firebaseAuth.signInWithEmailAndPassword(logEmailAddress, logPassword).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()){
-                        progressDialog.dismiss();
-                        Toast.makeText(getApplicationContext(), "Login successful", Toast.LENGTH_SHORT).show();
-                        FirebaseUser manager = firebaseAuth.getCurrentUser();
-                        updateUI(manager);
-                        startActivity(new Intent(Login.this, Home.class));
-                    }else {
-                        progressDialog.dismiss();
-                        updateUI(null);
-                        Toast.makeText(Login.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
-                    }
+            firebaseAuth.signInWithEmailAndPassword(logEmailAddress, logPassword).addOnCompleteListener(this, task -> {
+                if (task.isSuccessful()){
+                    progressDialog.dismiss();
+                    Toast.makeText(getApplicationContext(), "Login successful", Toast.LENGTH_SHORT).show();
+                    FirebaseUser manager = firebaseAuth.getCurrentUser();
+                    updateUI();
+                    startActivity(new Intent(Login.this, Home.class));
+                }else {
+                    progressDialog.dismiss();
+                    updateUI();
+                    Toast.makeText(Login.this, ""+task.getException(), Toast.LENGTH_SHORT).show();
                 }
             });
         }
     }
 
-    private void updateUI(FirebaseUser manager) {
+    private void updateUI() {
     }
     
 }
---
+++ app/src/main/java/com/example/ulendoapp/Signup.java
@@ -0,0 +1,68 @@
+package com.example.ulendoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
+public class Signup extends AppCompatActivity {
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+
+    MaterialButton nextBtn;
+    TextInputEditText textFirstName, textLastName, textPhoneNumber;
+    AutoCompleteTextView textGender;
+    TextInputLayout materialFistName, materialLastName, materialPhoneNumber, materialGender;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        textFirstName = findViewById(R.id.textFirstName);
+        textLastName = findViewById(R.id.textLastName);
+        textPhoneNumber = findViewById(R.id.textPhoneNumber);
+        textGender = findViewById(R.id.textGender);
+        materialFistName = findViewById(R.id.materialFistName);
+        materialLastName = findViewById(R.id.materialLastName);
+        materialPhoneNumber = findViewById(R.id.materialPhoneNumber);
+        materialGender = findViewById(R.id.materialGender);
+        nextBtn = findViewById(R.id.nextBtn);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performSignUp();
+            }
+        });
+    }
+    private void performSignUp(){
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        String fistName = Objects.requireNonNull(textFirstName.getText()).toString();
+        String lastName = Objects.requireNonNull(textLastName.getText()).toString();
+        String phoneNumber = Objects.requireNonNull(textPhoneNumber.getText()).toString();
+        String gender = textGender.getText().toString();
+
+        if (fistName.isEmpty()){
+            materialFistName.setError("Please enter first name");
+        }else if (lastName.isEmpty()){
+            materialLastName.setError("Please enter last name");
+        }else if (phoneNumber.isEmpty()){
+            materialPhoneNumber.setError("Please enter phone number");
+        }else if (gender.isEmpty()){
+            materialGender.setError("Select gender");
+        }
+
+    }
+}
