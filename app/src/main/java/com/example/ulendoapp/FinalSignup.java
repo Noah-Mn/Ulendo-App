e9f0c41ec04c724a4697ddc227cd393193379cdd
Banthapo <Bsc-11-16@cc.ac.mw>
2022-01-20 00:57:15 +0200

    connecting signup page with home page

--- app/src/main/java/com/example/ulendoapp/FinalSignup.java
+++ app/src/main/java/com/example/ulendoapp/FinalSignup.java
@@ -2,24 +2,31 @@ package com.example.ulendoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class FinalSignup extends AppCompatActivity {
-    private String firstName;
-    private String lastName;
-    private String phoneNumber;
-    private String gender;
-    private String email;
-    private String password;
-    private String confirmPassword;
+    private String firstName, lastName, phoneNumber, gender;
+    private String email, password, confirmPassword;
     private Button signupBtn;
+    private TextInputEditText textEmail, textPassword, textConfirmPassword;
+    private TextInputLayout materialEmail, materialPassword, materialConfirmPassword;
+    private ProgressDialog progressDialog;
+    private String emailPattern = "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]|[\\w-]{2,}))@"
+            + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+            + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+            + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+            + "[0-9]{1,2}|25[0-5]|2[0-4][0-9]))|"
+            + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +43,46 @@ public class FinalSignup extends AppCompatActivity {
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentSignup = new Intent(FinalSignup.this, Home.class);
+                if(validateFinalForm()){
+                    Intent intentSignup = new Intent(FinalSignup.this, Home.class);
+                    Toast.makeText(FinalSignup.this, "Successfully Signed up", Toast.LENGTH_LONG).show();
+                    startActivity(intentSignup);
+                }
             }
         });
+    }
+
+    private boolean validateFinalForm(){
+        materialEmail = findViewById(R.id.materialEmailAddress);
+        materialPassword = findViewById(R.id.materialPassword);
+        materialConfirmPassword = findViewById(R.id.materialConfirmPassword);
+        textEmail = findViewById(R.id.textEmailAddress);
+        textPassword = findViewById(R.id.textPassword);
+        textConfirmPassword = findViewById(R.id.textConfirmPassword);
+
+        email = textEmail.getText().toString();
+        password = textPassword.getText().toString();
+        confirmPassword = textConfirmPassword.getText().toString();
+
+        boolean valid = false;
+
+        try {
+            if (email.isEmpty()) {
+                materialEmail.setError("Please enter email address");
+            } else if(!email.matches(emailPattern)) {
+                materialEmail.setError("Please enter a valid email address");
+            } else if (password.isEmpty() && password.length() <= 3) {
+                materialPassword.setError("Password must be more than 3 characters");
+                if(!password.equals(confirmPassword)){
+                    materialPassword.setError("Passwords do not match");
+                }
+            } else if (!password.equals(confirmPassword)) {
+                     materialConfirmPassword.setError("passwords do not match");
+            } else{
+                valid = true;
+            }
+        }catch(Exception e){}
+        return valid;
     }
 
 
