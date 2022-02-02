e7d821e5fa04aa0b84ea0ed17ef9729c6efa8799
BSC_COM_09_17 <bsc-com-09-17@cc.ac.mw>
2022-02-02 23:48:47 +0200

    implementing home page of driver

--- app/src/main/java/com/example/ulendoapp/fragment_driver_home.java
+++ app/src/main/java/com/example/ulendoapp/fragment_driver_home.java
@@ -2,8 +2,10 @@ package com.example.ulendoapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,8 +15,14 @@ import android.view.ViewGroup;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.Objects;
 
 public class fragment_driver_home extends Fragment {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
     public fragment_driver_home(){
 
     }
@@ -22,8 +30,9 @@ public class fragment_driver_home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_driver_home, container, false);
-
+        View view = inflater.inflate(R.layout.fragment_driver_home, container, false);
+        addFragment(view);
+        return view;
     }
 
     @Override
@@ -31,11 +40,23 @@ public class fragment_driver_home extends Fragment {
         setHasOptionsMenu(true);
         super.onCreate(savedInstance);
 
+
+
+
     }
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+    public void onCreateOptionsMenu(Menu menu, @NonNull MenuInflater inflater){
 
         super.onCreateOptionsMenu(menu, inflater);
+    }
+    private  void addFragment(View view){
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager = view.findViewById(R.id.viewPager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new find_rides(), "Find Ride");
+        adapter.addFragment(new offer_rides(), "Offer Ride");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
 }
--- app/src/main/java/com/example/ulendoapp/offer_rides.java
+++ app/src/main/java/com/example/ulendoapp/offer_rides.java
@@ -3,56 +3,30 @@ package com.example.ulendoapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link offer_rides#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.Objects;
+
 public class offer_rides extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    MaterialCardView find_rides;
+    FrameLayout driver_home;
     public offer_rides() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment offer_rides.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static offer_rides newInstance(String param1, String param2) {
-        offer_rides fragment = new offer_rides();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -61,4 +35,5 @@ public class offer_rides extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_offer_rides, container, false);
     }
+
 }
