51ea00cc0f25fc5962aa8f0ec306b20a89a1c486
BSC_COM_09_17 <bsc-com-09-17@cc.ac.mw>
2022-02-03 16:07:28 +0200

    Driver bottom navigation

--- app/src/main/java/com/example/ulendoapp/Profile.java
+++ app/src/main/java/com/example/ulendoapp/Profile.java
@@ -1,14 +1,22 @@
 package com.example.ulendoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class Profile extends AppCompatActivity {
 
+    RecyclerView where_car_model_goes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        where_car_model_goes = findViewById(R.id.where_car_model_goes);
+       LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+       layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+       where_car_model_goes.setLayoutManager(layoutManager);
     }
 }
