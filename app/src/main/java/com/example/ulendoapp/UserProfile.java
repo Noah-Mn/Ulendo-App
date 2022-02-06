fb2e3e15c75846b988356ef1b9f06d187f9b9a16
Noah <bsc-com-09-17@cc.ac.mw>
2022-02-06 19:22:27 +0200

    user profile

--- app/src/main/java/com/example/ulendoapp/UserProfile.java
+++ app/src/main/java/com/example/ulendoapp/UserProfile.java
@@ -1,20 +1,43 @@
 package com.example.ulendoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class UserProfile extends AppCompatActivity {
     ImageView profile_back;
+    MaterialTextView user_name, user_number;
+    FirebaseFirestore db;
+    FirebaseAuth auth;
+    FirebaseUser firebaseUser;
+    private final String TAG = "User Profile";
+    String firstName, lastName, number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
         profile_back = findViewById(R.id.profile_back);
+        user_name = findViewById(R.id.user_name);
+        user_number = findViewById(R.id.user_number);
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        firebaseUser = auth.getCurrentUser();
 
         profile_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,4 +46,34 @@ public class UserProfile extends AppCompatActivity {
             }
         });
     }
+    public void getUserData(){
+        db.collection("Users")
+                .whereEqualTo("Email Address", getEmail())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                firstName = document.getString("First Name");
+                                lastName = document.getString("Surname");
+                                number = document.getString("Phone Number");
+
+                                user_name.setText(new StringBuilder().append(firstName).append(" ").append(lastName).toString());
+                                user_number.setText(number);
+
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+    public String getEmail(){
+        String emailAddress;
+        emailAddress = firebaseUser.getEmail();
+        return emailAddress;
+    }
+
 }
