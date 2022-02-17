0c8a7611aa2fd08ce352e8f43ea8265add1c716c
BSC_COM_09_17 <bsc-com-09-17@cc.ac.mw>
2022-02-17 03:56:34 +0200

    some more corrections.....somethings were not done properly bro

--- app/src/main/java/com/example/ulendoapp/DriverProfile.java
+++ app/src/main/java/com/example/ulendoapp/DriverProfile.java
@@ -1,17 +1,33 @@
 package com.example.ulendoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class DriverProfile extends AppCompatActivity {
 
-
+    FirebaseAuth firebaseAuth;
+    FirebaseUser currentUser;
+    FirebaseFirestore db;
+    private final String TAG = "Driver Profile";
+    String fName, lastName;
+    MaterialTextView textView;
     ImageView profile_back, edit_profile;
 
     @Override
@@ -25,7 +41,10 @@ public class DriverProfile extends AppCompatActivity {
 
        profile_back = findViewById(R.id.profile_back);
        edit_profile = findViewById(R.id.edit_profile);
-
+       firebaseAuth = FirebaseAuth.getInstance();
+       currentUser = firebaseAuth.getCurrentUser();
+       db = FirebaseFirestore.getInstance();
+        textView = findViewById(R.id.profile_name);
        edit_profile.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
@@ -39,5 +58,33 @@ public class DriverProfile extends AppCompatActivity {
                startActivity(new Intent(DriverProfile.this, HomeDriver.class));
            }
        });
+        getUserName();
+    }
+    public void getUserName(){
+        db.collection("Users")
+                .whereEqualTo("Email Address", getEmail())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                fName = document.getString("First Name");
+                                lastName = document.getString("Surname");
+
+                                textView.setText(new StringBuilder().append(fName).append(" ").append(lastName).toString());
+
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+    public String getEmail(){
+        String emailAddress;
+        emailAddress = currentUser.getEmail();
+        return emailAddress;
     }
 }
