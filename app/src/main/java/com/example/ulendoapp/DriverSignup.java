fe59d5164467ece7848bf7b0ce9a1440f7ac206c
banthapo <bsc-11-16@unima.ac.mw>
2022-02-21 22:54:51 +0200

    editting signup as driver form

--- app/src/main/java/com/example/ulendoapp/DriverSignup.java
+++ app/src/main/java/com/example/ulendoapp/DriverSignup.java
@@ -31,9 +31,10 @@ import java.util.Map;
 import java.util.Objects;
 
 public class DriverSignup extends AppCompatActivity {
-    private TextInputEditText textCarModel, textPhysicalAddress, textNumberPlate;
-    private TextInputLayout materialCarModel, materialPhysicalAddress, materialNumberPlate;
-    private String carModel, plateNumber, physicalAddress, email;
+    private TextInputEditText textVehicleBrand, textVehicleModel, textModelYear, textVehicleColor, textBookingType, textLicensePlate, textLicenseId;
+    private TextInputLayout materialVehicleBrand, materialVehicleModel, materialModelYear, materialVehicleColor,
+            materialBookingType, materialLicensePlate, materialLicenseId;
+    private String vBrand, vModel, vModelYr, vColor, vBookingType, vLicensePlate, vLicenseId;
     private Button submitBtn;
     private Boolean success;
     private FirebaseFirestore db;
@@ -49,37 +50,59 @@ public class DriverSignup extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         currentUser = auth.getCurrentUser();
 
-        materialCarModel = findViewById(R.id.materialCarModel);
-        materialNumberPlate = findViewById(R.id.materialNumberPlate);
-        materialPhysicalAddress = findViewById(R.id.materialPhysicalAddress);
-        textCarModel = findViewById(R.id.textCarModel);
-        textNumberPlate = findViewById(R.id.textNumberPlate);
-        textPhysicalAddress = findViewById(R.id.textPhysicalAddress);
+        textVehicleBrand = findViewById(R.id.text_vehicle_brand);
+        textVehicleModel = findViewById(R.id.text_vehicle_model);
+        textModelYear = findViewById(R.id.text_model_year);
+        textVehicleColor = findViewById(R.id.text_vehicle_color);
+        textBookingType =  findViewById(R.id.text_vehicle_booking_type);
+        textLicensePlate = findViewById(R.id.text_vehicle_licence_plate);
+        textLicenseId = findViewById(R.id.text_licence_id);
 
-        submitBtn = findViewById(R.id.submitBtn);
+        materialVehicleBrand = findViewById(R.id.material_signup_vehicle_brand);
+        materialVehicleModel = findViewById(R.id.material_signup_vehicle_model);
+        materialModelYear = findViewById(R.id.material_signup_model_year);
+        materialVehicleColor = findViewById(R.id.material_signup_vehicle_color);
+        materialBookingType  = findViewById(R.id.material_signup_booking_type);
+        materialLicensePlate = findViewById(R.id.material_signup_licence_plate);
+        materialLicenseId = findViewById(R.id.material_license_id);
+
+        submitBtn = findViewById(R.id.driver_register_btn);
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addDriver();
+                if(validateDriver()){
+                    addDriver();
+                }
             }
         });
     }
 
     private boolean validateDriver(){
         boolean valid = false;
+        vBrand = Objects.requireNonNull(textVehicleBrand.getText()).toString();
+        vModel = Objects.requireNonNull(textVehicleModel.getText()).toString();
+        vModelYr = Objects.requireNonNull(textModelYear.getText()).toString();
+        vColor = Objects.requireNonNull(textVehicleColor.getText()).toString();
+        vBookingType = Objects.requireNonNull(textBookingType.getText()).toString();
+        vLicensePlate = Objects.requireNonNull(textLicensePlate.getText()).toString();
+        vLicenseId = Objects.requireNonNull(textLicenseId.getText()).toString();
 
-        carModel = Objects.requireNonNull(textCarModel.getText()).toString();
-        plateNumber = Objects.requireNonNull(textNumberPlate.getText()).toString();
-        physicalAddress = Objects.requireNonNull(textPhysicalAddress.getText()).toString();
 
-        if (carModel.isEmpty()){
-            materialCarModel.setError("Please enter your car model");
-        }else if (plateNumber.isEmpty()){
-            materialNumberPlate.setError("Please enter plate number");
-        } else if (physicalAddress.isEmpty()) {
-            materialPhysicalAddress.setError("Please enter your address");
-        } else{
-            email = getEmail();
+        if (vBrand.isEmpty()){
+            materialVehicleBrand.setError("Please enter your car brand");
+        }else if (vModel.isEmpty()){
+            materialVehicleModel.setError("Please enter car model");
+        } else if (vModelYr.isEmpty()) {
+            materialModelYear.setError("Please enter model year");
+        } else if (vColor.isEmpty()) {
+            materialVehicleColor.setError("Please enter vehicle color");
+        } else if (vBookingType.isEmpty()) {
+            materialBookingType.setError("Please select your booking type");
+        }else if (vLicensePlate.isEmpty()) {
+            materialLicensePlate.setError("Please enter license number");
+        } else if (vLicenseId.isEmpty()) {
+            materialLicenseId.setError("Please enter license ID");
+        }else{
             valid = true;
         }
 
@@ -87,27 +110,23 @@ public class DriverSignup extends AppCompatActivity {
     }
 
     private void addDriver(){
-        if(validateDriver()){
             db = FirebaseFirestore.getInstance();
             FirebaseDatabase.getInstance().setPersistenceEnabled(true);
             Map<String, Object> driver = new HashMap<>();
 
-            driver.put("Car Model", carModel);
-            driver.put("Plate Number", plateNumber);
-            driver.put("Physical Address", physicalAddress);
-            driver.put("Email Address", email);
-            driver.put("Nation ID", "N/A");
-            driver.put("Drivers License ID", "N/A");
+            driver.put("Vehicle Brand", vBrand);
+            driver.put("Vehicle Model", vModel);
+            driver.put("Model Year", vModelYr);
+            driver.put("Vehicle Color", vColor);
+            driver.put("Booking Type", vBookingType);
+            driver.put("License Plate", vLicensePlate);
+            driver.put("License ID", vLicenseId);
             driver.put("Driver Status", "N/A");
-            driver.put("Current Location", "N/A");
-            driver.put("Destination", "N/A");
-            driver.put("Pickup Time", "N/A");
-            driver.put("ETA", "N/A");
-            driver.put("Ride as Passenger", "N/A");
-            driver.put("Ride as Driver", "N/A");
+            driver.put("Rides as Passenger", "N/A");
+            driver.put("Rides as Driver", "N/A");
             driver.put("Rating", "N/A");
 
-            db.collection("Drivers")
+            db.collection("Driver Vehicles")
                     .add(driver)
                     .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                         @Override
@@ -115,18 +134,16 @@ public class DriverSignup extends AppCompatActivity {
                             Log.d(TAG, "inserted successfully");
                             changeStatus();
                             DriverSignup.this.startActivity(new Intent(DriverSignup.this, HomeDriver.class));
-//                            Toast.makeText(DriverSignup.this, "Successfully inserted", Toast.LENGTH_LONG).show();
+                            Toast.makeText(DriverSignup.this, "Successfully inserted", Toast.LENGTH_LONG).show();
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
                         @Override
                         public void onFailure(@NonNull Exception e) {
                             Log.d(TAG, "error! failed");
-//                            Toast.makeText(DriverSignup.this, "Unsuccessfully inserted", Toast.LENGTH_LONG).show();
+                            Toast.makeText(DriverSignup.this, "Unsuccessfully inserted", Toast.LENGTH_LONG).show();
                         }
                     });
-
-        }
     }
 
     private String getEmail(){
@@ -137,7 +154,7 @@ public class DriverSignup extends AppCompatActivity {
 
     private void changeStatus(){
         db.collection("Users")
-                .whereEqualTo("Email Address", email)
+                .whereEqualTo("Email Address", getEmail())
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
