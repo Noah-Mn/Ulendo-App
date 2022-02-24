f63139d04f5050fa135b7499e6a32d37613e2a5d
banthapo <bsc-11-16@unima.ac.mw>
2022-02-24 06:26:34 +0200

    editting things and some..

--- app/src/main/java/com/example/ulendoapp/BookRideViewHolder.java
+++ app/src/main/java/com/example/ulendoapp/BookRideViewHolder.java
@@ -8,18 +8,16 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.textview.MaterialTextView;
-
 public class BookRideViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
     public ImageView imageView;
-    public TextView driverName, carDetail, tripStartPoint, tripDestination, tripState;
+    public TextView driverName, carDetail, tripStartPoint, tripDestination, dateTime;
     public BookRideAdapter.OnTripClickListener onTripClickListener;
     public CardView tripLayout;
 
     public BookRideViewHolder(@NonNull View itemView, BookRideAdapter.OnTripClickListener onTripClickListener) {
         super(itemView);
         driverName = itemView.findViewById(R.id.name_of_driver);
-        tripState = itemView.findViewById(R.id.trip_status);
+        dateTime = itemView.findViewById(R.id.trip_status);
         carDetail = itemView.findViewById(R.id.car_details);
         tripStartPoint = itemView.findViewById(R.id.book_trip_starting_point);
         tripDestination = itemView.findViewById(R.id.book_trip_end_point);
