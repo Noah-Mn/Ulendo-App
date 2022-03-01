f4848c55e71f1d64ca35adaba41815066c365d3e
BSC_COM_09_17 <bsc-com-09-17@cc.ac.mw>
2022-03-02 01:04:50 +0200

    Chat continuation

--- app/src/main/java/com/example/ulendoapp/ChatActivity.java
+++ app/src/main/java/com/example/ulendoapp/ChatActivity.java
@@ -31,6 +31,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 public class ChatActivity extends BaseActivity {
     private ActivityChatBinding binding;
@@ -42,6 +43,7 @@ public class ChatActivity extends BaseActivity {
     FirebaseAuth auth;
     FirebaseUser currentUser;
     private String conversationId = null;
+    private Boolean isReceiverAvailable = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,6 +107,30 @@ public class ChatActivity extends BaseActivity {
                 });
 
     }
+
+    private void listenAvailabilityOfReceiver(){
+        database.collection(Constants.KEY_COLLECTION_USERS).document(
+                receiverUser.id
+        ).addSnapshotListener(ChatActivity.this, (value, error) -> {
+            if (error != null){
+                return;
+            }
+            if (value != null){
+                if (value.getLong(Constants.KEY_AVAILABILITY) != null){
+                    int availability = Objects.requireNonNull(
+                            value.getLong(Constants.KEY_AVAILABILITY)
+                    ).intValue();
+                    isReceiverAvailable = availability == 1;
+                }
+            }
+            if (isReceiverAvailable){
+                binding.textAvailability.setVisibility(View.VISIBLE);
+            }else {
+                binding.textAvailability.setVisibility(View.GONE);
+            }
+        });
+    }
+
     private void init(){
         preferenceManager = new PreferenceManager(getApplicationContext());
         chatMessages = new ArrayList<>();
@@ -228,4 +254,9 @@ public class ChatActivity extends BaseActivity {
         }
     };
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        listenAvailabilityOfReceiver();
+    }
 }
