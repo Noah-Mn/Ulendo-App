e1eb99f2c14abe01735226e60b8f78436c348fca
banthapo <bsc-11-16@unima.ac.mw>
2022-03-07 16:50:34 +0200

    clustering and editing ...

--- app/src/main/java/com/example/ulendoapp/adapters/UsersAdapter.java
+++ app/src/main/java/com/example/ulendoapp/adapters/UsersAdapter.java
@@ -44,7 +44,7 @@ public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UserViewHold
 
     @Override
     public int getItemCount() {
-        return 4;
+        return users.size();
     }
 
     class UserViewHolder extends RecyclerView.ViewHolder{
