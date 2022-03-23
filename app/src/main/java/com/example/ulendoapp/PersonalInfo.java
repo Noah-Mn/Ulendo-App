bf03d2e2ca2e6fc3c025be541285557349cc9860
Noah-Mn <bsc-com-09-17@unima.ac.mw>
2022-03-24 01:04:46 +0200

    Personal Infor has been implemented but not run on emulator yet

--- app/src/main/java/com/example/ulendoapp/PersonalInfo.java
+++ app/src/main/java/com/example/ulendoapp/PersonalInfo.java
@@ -1,65 +1,42 @@
-package com.example.ulendoapp.activityClasses;
+package com.example.ulendoapp;
 
-import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
-import com.example.ulendoapp.PersonalInfo;
-import com.example.ulendoapp.databinding.ActivityDriverProfileBinding;
+import com.example.ulendoapp.databinding.ActivityPersonalInfoBinding;
 import com.example.ulendoapp.utilities.Constants;
-import com.example.ulendoapp.utilities.PreferenceManager;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-public class DriverProfile extends AppCompatActivity {
-    ActivityDriverProfileBinding binding;
-    PreferenceManager preferenceManager;
-
+public class PersonalInfo extends AppCompatActivity {
+    ActivityPersonalInfoBinding binding;
     FirebaseAuth firebaseAuth;
     FirebaseUser currentUser;
     FirebaseFirestore db;
-    private final String TAG = "Driver Profile";
-    String fName, lastName, encodedImage;
+    String fName, lastName, encodedImage,gender, email, birthday, phoneNumber, nationalID, physicalAddr;
+    private final String TAG = "Personal Info";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        binding = ActivityDriverProfileBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
+        binding = ActivityPersonalInfoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        preferenceManager = new PreferenceManager(getApplicationContext());
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        currentUser = firebaseAuth.getCurrentUser();
 
-       LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-       layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-
-       firebaseAuth = FirebaseAuth.getInstance();
-       currentUser = firebaseAuth.getCurrentUser();
-       db = FirebaseFirestore.getInstance();
-
-       binding.editProfile.setOnClickListener(v -> {
-           Intent intent = new Intent(DriverProfile.this, EditDriverProfile.class);
-           startActivity(intent);
-       });
-
-       binding.aLotOfStuff.setOnClickListener(view -> {
-           Intent intent = new Intent(DriverProfile.this, PersonalInfo.class);
-           startActivity(intent);
-       });
-
-       binding.profileBack.setOnClickListener(v -> startActivity(
-               new Intent(DriverProfile.this, HomeDriver.class)));
-        getUserName();
+        getInformation();
     }
 
-    public void getUserName(){
+    public void getInformation(){
         db.collection("Users")
                 .whereEqualTo("Email Address", getEmail())
                 .get()
@@ -69,11 +46,23 @@ public class DriverProfile extends AppCompatActivity {
                             Log.d(TAG, document.getId() + " => " + document.getData());
                             fName = document.getString("First Name");
                             lastName = document.getString("Surname");
+                            email = document.getString("Email Address");
+                            gender = document.getString("Gender");
+                            phoneNumber = document.getString("Phone Number");
+                            birthday = document.getString("Date of Birth");
+                            nationalID = document.getString("National ID");
+                            physicalAddr = document.getString("Physical Address");
                             encodedImage = document.getString(Constants.KEY_IMAGE);
                             byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
                             Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
                             binding.profileImage.setImageBitmap(bitmap);
-                            binding.profileName.setText(new StringBuilder().append(fName).append(" ").append(lastName).toString());
+                            binding.infoDisplayName.setText(new StringBuilder().append(fName).append(" ").append(lastName).toString());
+                            binding.infoAddress.setText(physicalAddr);
+                            binding.infoBirthday.setText(birthday);
+                            binding.infoID.setText(nationalID);
+                            binding.infoEmail.setText(email);
+                            binding.infoNumber.setText(phoneNumber);
+                            binding.infoGender.setText(gender);
                         }
                     } else {
                         Log.d(TAG, "Error getting documents: ", task.getException());
--- app/src/main/java/com/example/ulendoapp/activityClasses/UserProfile.java
+++ app/src/main/java/com/example/ulendoapp/activityClasses/UserProfile.java
@@ -1,91 +1,74 @@
 package com.example.ulendoapp.activityClasses;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
 
-import com.example.ulendoapp.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textview.MaterialTextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ulendoapp.PersonalInfo;
+import com.example.ulendoapp.databinding.ActivityUserProfileBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.makeramen.roundedimageview.RoundedImageView;
 
 public class UserProfile extends AppCompatActivity {
-    ImageView profile_back, user_edit_profile;
-    MaterialTextView user_name, user_number;
+    ActivityUserProfileBinding binding;
     FirebaseFirestore db;
     FirebaseAuth auth;
     FirebaseUser firebaseUser;
-    RoundedImageView userprofilePic;
     private final String TAG = "User Profile";
     String firstName, lastName, number, encodedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile);
-        profile_back = findViewById(R.id.profile_back);
-        user_name = findViewById(R.id.user_name);
-        user_number = findViewById(R.id.user_number);
+        binding = ActivityUserProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
         db = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
         firebaseUser = auth.getCurrentUser();
-        user_edit_profile = findViewById(R.id.user_edit_profile);
-        userprofilePic = findViewById(R.id.user_profile_pic);
+
         getUserData();
 
-        profile_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(UserProfile.this, HomeUser.class));
-            }
-        });
+        binding.profileBack.setOnClickListener(v -> startActivity(
+                new Intent(UserProfile.this, HomeUser.class)));
 
-        user_edit_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(UserProfile.this, EditUserProfile.class));
-            }
+        binding.userEditProfile.setOnClickListener(v -> startActivity(
+                new Intent(UserProfile.this, EditUserProfile.class)));
+
+        binding.aLotOfStuff.setOnClickListener(view -> {
+            Intent intent = new Intent(UserProfile.this, PersonalInfo.class);
+            startActivity(intent);
         });
     }
     public void getUserData(){
         db.collection("Users")
                 .whereEqualTo("Email Address", getEmail())
                 .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                                firstName = document.getString("First Name");
-                                lastName = document.getString("Surname");
-                                number = document.getString("Phone Number");
-                                encodedImage = document.getString("Profile Pic");
-                                byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
-                                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                                userprofilePic.setImageBitmap(bitmap);
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Log.d(TAG, document.getId() + " => " + document.getData());
+                            firstName = document.getString("First Name");
+                            lastName = document.getString("Surname");
+                            number = document.getString("Phone Number");
+                            encodedImage = document.getString("Profile Pic");
+                            byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
+                            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                            binding.userProfilePic.setImageBitmap(bitmap);
 
-                                user_name.setText(new StringBuilder().append(firstName).append(" ").append(lastName).toString());
-                                user_number.setText(number);
+                            binding.userName.setText(new StringBuilder().append(firstName).append(" ").append(lastName).toString());
+                            binding.userNumber.setText(number);
 
-                            }
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
                         }
+                    } else {
+                        Log.d(TAG, "Error getting documents: ", task.getException());
                     }
                 });
     }
