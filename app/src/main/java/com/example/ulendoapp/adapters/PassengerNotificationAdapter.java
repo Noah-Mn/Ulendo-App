d4186a9385fbb3ffff766d3e0b3f57f037be0716
Noah-Mn <bsc-com-09-17@unima.ac.mw>
2022-04-18 18:34:26 +0200

    Passenger Notifications

--- app/src/main/java/com/example/ulendoapp/adapters/PassengerNotificationAdapter.java
+++ app/src/main/java/com/example/ulendoapp/adapters/PassengerNotificationAdapter.java
@@ -1,12 +1,16 @@
 package com.example.ulendoapp.adapters;
 
+import android.content.ClipData;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.ulendoapp.R;
 import com.example.ulendoapp.models.NotificationModel;
 import com.example.ulendoapp.utilities.PreferenceManager;
 import com.google.firebase.auth.FirebaseAuth;
@@ -17,6 +21,9 @@ import java.util.List;
 
 public class PassengerNotificationAdapter extends RecyclerView.Adapter<PassengerNotificationAdapter.PassengerNotificationViewHolder> {
     public List<NotificationModel> passengerNotifications;
+    private static final int TYPE_ONE = 1;
+    private static final int TYPE_TWO = 2;
+    private static final int TYPE_THREE = 3;
 
     Context context;
     PreferenceManager preferenceManager;
@@ -24,20 +31,53 @@ public class PassengerNotificationAdapter extends RecyclerView.Adapter<Passenger
     FirebaseAuth auth;
     FirebaseUser currentUser;
 
+    public int getItemViewType(int position){
+        NotificationModel notificationModel = passengerNotifications.get(position);
+        if (notificationModel.getType() == NotificationModel.ItemType.ONE_ITEM){
+            return TYPE_ONE;
+        }else if (notificationModel.getType() == NotificationModel.ItemType.TWO_ITEM){
+            return TYPE_TWO;
+        }else if (notificationModel.getType() == NotificationModel.ItemType.THREE_ITEM){
+            return TYPE_THREE;
+        }else {
+            return -1;
+        }
+    }
+
+    public PassengerNotificationAdapter(List<NotificationModel> passengerNotifications, Context context) {
+        this.passengerNotifications = passengerNotifications;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public PassengerNotificationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        if (viewType == TYPE_ONE){
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notificatin_accepted, parent, false);
+            return new PassengerNotificationViewHolder(view);
+        }else if (viewType == TYPE_TWO){
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notification_rejected, parent, false);
+            return new PassengerNotificationViewHolder(view);
+        }else if (viewType == TYPE_THREE){
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notification_ignored, parent, false);
+            return new PassengerNotificationViewHolder(view);
+        }else {
+            throw new RuntimeException("The type has to be one or two or three");
+        }
     }
 
     @Override
     public void onBindViewHolder(@NonNull PassengerNotificationViewHolder holder, int position) {
+        switch (holder.getItemViewType()){
+            case TYPE_ONE:
 
+
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return passengerNotifications.size();
     }
 
     static class PassengerNotificationViewHolder extends RecyclerView.ViewHolder {
